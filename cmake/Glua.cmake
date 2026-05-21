c2d8da0ae5bf183ff3d12113673bef6a806b7471
Alexander Fasching <fasching.a91@gmail.com>
2026-05-21 15:12:09 +0200

    Fix glua tools path

--- cmake/Glua.cmake
+++ cmake/Glua.cmake
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set(GLUA_TOOLS "${CMAKE_CURRENT_LIST_DIR}/../tools")
+set(GLUA_TOOLS "${CMAKE_CURRENT_LIST_DIR}/../tools" CACHE STRING "Path to the tools directory" FORCE)
 
 # Create a target for a Glua header file.
 # * <inputs> is semicolon separated list of YAML schema files
 # * <output> is the generated header file
 function(glua_target target inputs output)
+    message("Glua tools directory: ${GLUA_TOOLS}")
     set(GLUA_GENERATE "${GLUA_TOOLS}/glua-generate.sh")
     file(GLOB GLUA_MODULE_FILES CONFIGURE_DEPENDS
         "${GLUA_TOOLS}/glua/*.py"
