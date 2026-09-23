f5669dccaeeb8cb5c58a23547dcbb1e1beaa398c
Alexander Fasching <fasching.a91@gmail.com>
2026-09-23 21:05:26 +0200

    Add package config

--- flake.nix
+++ flake.nix
@@ -15,6 +15,29 @@
       ];
 
       perSystem = { pkgs, ... }: {
+        # Package configuration.
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "glua";
+          version = "unstable";
+
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+            lua5_5
+            stb
+            (python3.withPackages (python-pkgs: [
+              python-pkgs.mako
+            ]))
+          ];
+        };
+
+        # Developer configuration.
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             clang-tools
