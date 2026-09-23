224abed4947a6b2861fd0105701989bcdc8759af
Alexander Fasching <fasching.a91@gmail.com>
2026-09-23 20:15:51 +0200

    Add flake.nix file

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A glue code generator for C and Lua.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang-tools
+            cmake
+            ninja
+            pkg-config
+            pre-commit
+            rustup
+          ];
+
+          buildInputs = with pkgs; [
+            lua5_5
+            stb
+            (python3.withPackages (python-pkgs: [
+              python-pkgs.mako
+            ]))
+          ];
+        };
+      };
+    };
+}
